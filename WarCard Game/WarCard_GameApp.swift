f4c98f8f2ec881234d231dfcaadbf30829104dfa
kmarion76 <kmarion76@gmail.com>
2022-04-01 09:06:22 -0400

    Initial Commit
    First commit from war card game repo

---
+++ WarCard Game/WarCard_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCard_GameApp.swift
+//  WarCard Game
+//
+//  Created by Kevin Marion on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WarCard_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
